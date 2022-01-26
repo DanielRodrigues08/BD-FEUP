8c226bf3328c7661a504a1a7c3d580f46fe8a5ba
DanielRodrigues202006562 <up202006562@up.pt>
2022-01-26 16:55:52 +0000

    Adoro BD

--- int6.sql
+++ int6.sql
@@ -4,5 +4,8 @@
 
 SELECT Circuit.name as GP, CarCircuitSetup.idCar, Engine.idComponent as idEngine, pace
 FROM Setup, CarCircuitSetup, Circuit, Engine
-WHERE Engine.idComponent in (SELECT idComponent FROM Component WHERE Component.idCar = CarCircuitSetup.idCar) AND
-	Circuit.idCircuit = CarCircuitSetup.idCircuit AND Setup.idSetup = CarCircuitSetup.idSetup AND  pace in (SELECT min(pace) FROM Setup);
+
+WHERE Engine.idComponent in (SELECT idComponent FROM Component WHERE Component.idCar = CarCircuitSetup.idCar) 
+	AND
+	Circuit.idCircuit = CarCircuitSetup.idCircuit AND Setup.idSetup = CarCircuitSetup.idSetup AND  
+	pace in (SELECT min(pace) FROM Setup);
