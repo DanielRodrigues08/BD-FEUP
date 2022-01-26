42d0c045ddacb78c6455bf37d073fc70652a202c
MoreiraP12 <up201905429@edu.fe.up.pt>
2022-01-26 16:59:34 +0000

    Pedro's triggers

--- trigger1delete.sql
+++ trigger1delete.sql
@@ -1,7 +1,26 @@
-CREATE TRIGGER deleteRelationDSI
+CREATE TRIGGER deleteDepartment
 AFTER DELETE ON Department
 FOR EACH ROW
 BEGIN
-	DELETE FROM 
-	WHERE departmentID = OLD.departmentID;
+	DELETE FROM InfrastructureDepartmentStaff
+	WHERE idDepartment = OLD.idDepartment;
+END;
+
+
+
+CREATE TRIGGER deleteInfrasturcture
+AFTER DELETE ON Infrastructure
+FOR EACH ROW
+BEGIN
+	DELETE FROM InfrastructureDepartmentStaff
+	WHERE idInfrastructure = OLD.idInfrastructure;
+END;
+
+
+CREATE TRIGGER deleteStaff
+AFTER DELETE ON Staff
+FOR EACH ROW
+BEGIN
+	DELETE FROM InfrastructureDepartmentStaff
+	WHERE idStaff = OLD.idStaff;
 END;
